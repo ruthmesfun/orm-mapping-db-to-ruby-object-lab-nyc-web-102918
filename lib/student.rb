ccdf05e97c783bb064ebb4650f70b547ca0c3c9f
ruthmesfun <ruth.mesfun@gmail.com>
2018-11-08 10:57:33 -0500

    All Done

--- lib/student.rb
+++ lib/student.rb
@@ -3,16 +3,33 @@ class Student
 
   def self.new_from_db(row)
     # create a new Student object given a row from the database
+    new_student = new
+    new_student.id  = row[0]
+    new_student.name = row[1]
+    new_student.grade = row[2]
+
+    new_student
   end
 
   def self.all
     # retrieve all the rows from the "Students" database
     # remember each row should be a new instance of the Student class
+    sql = <<-SQL
+      SELECT * FROM students
+    SQL
+
+    DB[:conn].execute(sql).map{|row| self.new_from_db(row)}
   end
 
   def self.find_by_name(name)
     # find the student in the database given a name
     # return a new instance of the Student class
+    sql = <<-SQL
+      SELECT * FROM students WHERE name = ? LIMIT 1
+    SQL
+
+    DB[:conn].execute(sql, name).map{|row| self.new_from_db(row)}.first
+
   end
   
   def save
@@ -40,4 +57,43 @@ class Student
     sql = "DROP TABLE IF EXISTS students"
     DB[:conn].execute(sql)
   end
+
+  def self.all_students_in_grade_9
+      sql = "SELECT * FROM students WHERE grade = 9"
+
+    DB[:conn].execute(sql).map{|row| self.new_from_db(row)}
+  end
+
+  def self.students_below_12th_grade
+    sql = "SELECT * FROM students WHERE grade < 12"
+
+    DB[:conn].execute(sql).map{|row| self.new_from_db(row)}
+  end
+
+  def self.first_X_students_in_grade_10(num)
+    sql = <<-SQL 
+          SELECT * FROM students WHERE grade = 10 LIMIT ?
+        SQL
+
+    DB[:conn].execute(sql, num).map{|row| self.new_from_db(row)}
+  end
+
+  def self.first_student_in_grade_10
+    sql = <<-SQL 
+          SELECT * FROM students WHERE grade = 10
+        SQL
+
+    DB[:conn].execute(sql).map{|row| self.new_from_db(row)}.first
+  end
+
+  def self.all_students_in_grade_X(num)
+    sql = <<-SQL 
+          SELECT * FROM students WHERE grade = ?
+        SQL
+
+    DB[:conn].execute(sql,num).map{|row| self.new_from_db(row)}
+  end
+
+
+
 end
